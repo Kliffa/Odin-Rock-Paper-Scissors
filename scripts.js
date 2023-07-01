67d3a54a5150f46461734bcca19843531bb3ad2f
kliffa <knielsvankleef@gmail.com>
2023-07-01 19:57:05 +0200

    Expand function playRound for all scenarios

--- scripts.js
+++ scripts.js
@@ -22,16 +22,24 @@ function getComuterChoice(){
 
 function playRound(playerSelection, computerSelection){
     //first make sure the playerSelection is case=insensitive by translating the input to a lower case variant.
-    console.log(playerSelection + " " + computerSelection)
+    console.log(playerSelection + " " + computerSelection) // call this out to verify whether the if statement below works
     if (playerSelection==computerSelection){
         return "It's a draw. You picked the same as the computer!";
-    } else if (playerSelection =="rock" && computerSelection == "paper") {
+    } else if (playerSelection =="rock" && computerSelection == "paper") { 
         return "You lose! Paper beats rock!";
     } else if (playerSelection =="rock"&& computerSelection == "scissors" ){
         return "You win! Rock beats scissors!";
+    } else if (playerSelection =="paper"&& computerSelection == "scissors" ){
+        return "You lose! Scissors beat paper";
+    } else if (playerSelection =="paper"&& computerSelection == "rock" ){
+        return "You win! Paper beats rock!";
+    } else if (playerSelection =="scissors"&& computerSelection == "rock" ){
+        return "You lose! Rock beats scissors!";
+    } else if (playerSelection =="scissors"&& computerSelection == "paper" ){
+        return "You win! Scissors beat paper!";
     }
 }
 
-const playerSelection = "rock";
+const playerSelection = "scissors";
 const computerSelection = getComuterChoice();
 console.log(playRound(playerSelection, computerSelection));
